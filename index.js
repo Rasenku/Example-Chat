296b07d724d5926b0a8ada573518f827236d94e8
Dahveyea <dahveyea.cowan@gmail.com>
2021-05-06 17:17:48 -0700

    Added more functionality

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
+  res.sendFile(`${__dirname}/index.html`)
+})
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+io.on('connection', socket => {
+  let current_user
+  socket.on('name', name => {
+    current_user = name
+    io.emit('broadcast', `${current_user} has entered the chat`)
+
+    socket.on('disconnect', () => {
+      io.emit('broadcast', `${current_user} has left the chat`)
+    })
+  })
+  socket.on('chat message', msg => {
+    io.emit('chat message', `${current_user}: ${msg}`)
+  })
+})
+
+http.listen(port, () => {
+  console.log(`Listening on *:${port}`)
+})
